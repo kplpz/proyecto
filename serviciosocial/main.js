fcb0b56876c15bfa39c3f3a709a7c46aef540288
Karen Lopez <pl15012@ues.edu.sv>
2023-10-16 02:28:24 -0600

    pop

--- serviciosocial/main.js
+++ serviciosocial/main.js
@@ -3236,8 +3236,7 @@ __webpack_require__.r(__webpack_exports__);
 const environment = {
     production: false,
     // urlG : 'http://localhost:8083'
-    //  urlG : 'http://52.90.201.74:8083'
-    urlG: 'https://karenlpz2023.me:8083'
+    urlG: 'http://52.90.201.74:8083'
 };
 
 
